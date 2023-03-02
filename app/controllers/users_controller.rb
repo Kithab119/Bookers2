7526fd0759b63f5d9fc3eca857bddbdcfa8af150
EC2 Default User <ec2-user@ip-172-31-37-250.ap-northeast-1.compute.internal>
2023-03-02 14:49:20 +0000

    [check10]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,24 @@ class UsersController < ApplicationController
     @book=Book.new
   end
 
+  def edit
+    @user=User.find(params[:id])
+  end
+
+  def update
+    @user=User.find(params[:id])
+    @user.user_id = current_user.id
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
   def index
     @users=User.all
   end
+
+  private
+   def user_params
+     params.require(:user).permit(:name,:image,:introduction)
+   end
 
 end
