2db5113dff2f44466ce1c8dfb0efd3bb6d19fd94
EC2 Default User <ec2-user@ip-172-31-37-250.ap-northeast-1.compute.internal>
2023-03-07 12:17:16 +0000

    [Finish.]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+ before_action :is_matching_login_user, only: [:edit, :update]
+
   def show
     @user=User.find(params[:id])
     @book=Book.new
@@ -7,18 +9,10 @@ class UsersController < ApplicationController
   end
 
   def edit
-    user_id = params[:id].to_i
-    unless user_id == current_user.id
-      redirect_to user_path
-    end
     @user=User.find(params[:id])
   end
 
   def update
-    user_id = params[:id].to_i
-    unless user_id == current_user.id
-      redirect_to user_path
-    end
     @user=User.find(params[:id])
     if @user.update(user_params)
        flash[:notice]="You have updated user successfully."
@@ -35,6 +29,13 @@ class UsersController < ApplicationController
   private
    def user_params
      params.require(:user).permit(:name,:profile_image,:introduction)
+   end
+
+   def is_matching_login_user
+    user_id = params[:id].to_i
+    unless user_id == current_user.id
+      redirect_to user_path(current_user.id)
+    end
    end
 
 end
